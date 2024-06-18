d678407d78d710357728cec24a0948a09996c638
Nishiminhu <minhukensetsu@gmail.com>
2024-06-18 18:00:31 +0700

    Final revision.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginForm from './compoments/login/LoginForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginForm />} exact />
+        <Route path="/forgot-password" element={() => <h1>Forgot Password</h1>} />
+        <Route path="/register" element={() => <h1>Register</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
